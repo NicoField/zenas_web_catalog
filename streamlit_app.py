8248f490f20e3c1b86c91a9142c3f181358e9cf7
NicoField <74797059+NicoField@users.noreply.github.com>
2023-05-31 15:52:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ streamlit.title("Zena's Amazing Athleisure Catalog")
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
-my_cur.execute("select color_or style from catalog_for_website")
+my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
 
 df = pandas.DataFrame(my_catalog)
@@ -16,8 +16,4 @@ streamlit.write(df)
 color_list = df[0].values.tolist()
 option = streamlit.selectbox("Pick a sweatsuit color or style:", list(color_list), ['Avocado', 'Strawberries'])
 
-
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
 
